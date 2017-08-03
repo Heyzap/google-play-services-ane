15da5862f6ed547e877c56e3d955ab49ddca5126
Emory Petermann <emory@heyzap.com>
2017-08-03 13:38:05 -0700

    fix resource location to be valid

--- android/libs/prep.sh
+++ android/libs/prep.sh
@@ -4,5 +4,7 @@ for filename in *.aar; do
   base=${filename%.aar}
   unzip ${filename} classes.jar
   unzip ${filename} "res/*" -d res/${base}
+  mv res/${base}/res/** res/${base}/
+  rm -r res/${base}/res
   mv classes.jar ${base}.jar
 done
